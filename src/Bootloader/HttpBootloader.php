12af1c87045d4130c72351c7d97bf605efad002a
Kirill Ostashko <ko6717@gmail.com>
2022-07-30 19:25:00 +0300

    Added bootloader

---
+++ src/Bootloader/HttpBootloader.php
@@ -0,0 +1,13 @@
+<?php
+
+use Spiral\Boot\Bootloader\Bootloader;
+use Spiral\Boot\KernelInterface;
+use Spiral\Core\FactoryInterface;
+
+class HttpBootloader extends Bootloader
+{
+    public function start(KernelInterface $kernel, FactoryInterface $factory): void
+    {
+        $kernel->addDispatcher($factory->make(Dispatcher::class));
+    }
+}
